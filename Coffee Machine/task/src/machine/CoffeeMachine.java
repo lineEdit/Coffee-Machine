0a5fe8c248f22f5f57cb563bafce2474ccde00f6
lineedit_work <lineedit@gmail.com>
2019-08-23 15:58:34 +0500

    Stage 5 end.

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -2,32 +2,46 @@ package machine;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        CupsMachine cupsMachine = new CupsMachine(1200,
+        CupsMachine cupsMachine = new CupsMachine(
+                400,
                 540,
                 120,
                 9,
                 550);
 
-        cupsMachine.itHas();
         Input input = Input.getInstance();
-        switch (input.getString("Write action (buy, fill, take): ")) {
-            case "buy":
-                cupsMachine.buy(input.getInt("What do you want to buy? " +
-                        "1 - espresso, " +
-                        "2 - latte, " +
-                        "3 - cappuccino: "));
-                break;
-            case "fill":
-                cupsMachine.fill(
-                        input.getInt("Write how many ml of water do you want to add: "),
-                        input.getInt("Write how many ml of milk do you want to add: "),
-                        input.getInt("Write how many grams of coffee beans do you want to add: "),
-                        input.getInt("Write how many disposable cups of coffee do you want to add: ")
-                        );
-                break;
-            case "take":
-                cupsMachine.take();
-                break;
+        boolean run = true;
+        while (run) {
+            switch (input.getString("\nWrite action (buy, fill, take, remaining, exit): ")) {
+                case "buy":
+                    String str = input.getString("What do you want to buy? " +
+                            "1 - espresso, " +
+                            "2 - latte, " +
+                            "3 - cappuccino, " +
+                            "back - to main menu: ");
+                    if (!str.equals("back")) {
+                        cupsMachine.buy(Integer.parseInt(str));
+                    }
+                    break;
+                case "fill":
+                    cupsMachine.fill(
+                            input.getInt("\nWrite how many ml of water do you want to add: "),
+                            input.getInt("Write how many ml of milk do you want to add: "),
+                            input.getInt("Write how many grams of coffee beans do you want to add: "),
+                            input.getInt("Write how many disposable cups of coffee do you want to add: ")
+                    );
+                    System.out.println();
+                    break;
+                case "take":
+                    cupsMachine.take();
+                    break;
+                case "remaining":
+                    cupsMachine.remaining();
+                    break;
+                case "exit":
+                    run = false;
+                    break;
+            }
         }
     }
 }
--- Coffee Machine/task/src/machine/Cups.java
+++ Coffee Machine/task/src/machine/Cups.java
@@ -55,48 +55,41 @@ class CupsMachine extends Cups{
         this.milk += milk;
         this.coffeeBeans += coffeeBeans;
         this.disposableCups += disposableCups;
-        itHas();
     }
 
     public void take() {
-        System.out.println("I gave you $" + money);
+        System.out.println("\n\nI gave you $" + money);
         money = 0;
-        itHas();
     }
 
     private boolean getOneCups(int type) {
         Cups cups = getCoffee(type);
-        boolean enough = true;
         if (water - cups.water >= 0) {
             water -= cups.water;
         } else {
-            enough = false;
+            System.out.println("Sorry, not enough water!");
+            return false;
         }
         if (milk - cups.milk >= 0) {
             milk -= cups.milk;
         } else {
-            enough = false;
+            System.out.println("Sorry, not enough milk!");
+            return false;
         }
         if (coffeeBeans - cups.coffeeBeans >= 0) {
             coffeeBeans -= cups.coffeeBeans;
         } else {
-            enough = false;
+            System.out.println("Sorry, not enough coffee beans!");
+            return false;
         }
         if (disposableCups - 1 >= 0) {
             disposableCups--;
         } else {
-            enough = false;
+            System.out.println("Sorry, not enough disposable cups!");
+            return false;
         }
-        return enough;
+        return true;
     }
-
-//    int getAllCups(Cups cups) {
-//        int cupsExist = 0;
-//        while (getOneCups(cups)) {
-//            cupsExist++;
-//        }
-//        return cupsExist;
-//    }
 
     public void addMoney(int type) {
         switch (type) {
@@ -114,11 +107,8 @@ class CupsMachine extends Cups{
 
     public void buy(int type) {
         if (getOneCups(type)) {
-//            System.out.println("Yes, I can make that amount of coffee");
+            System.out.println("I have enough resources, making you a coffee!");
             addMoney(type);
-            itHas();
-        } else {
-            System.out.println("No, I can make cup of coffee");
         }
     }
 
@@ -130,119 +120,12 @@ class CupsMachine extends Cups{
         return money;
     }
 
-    public void itHas() {
-        System.out.println("The coffee machine has:\n" +
+    public void remaining() {
+        System.out.println("\nThe coffee machine has:\n" +
                 getWater() + " of water\n" +
                 getMilk() + " of milk\n" +
                 getCoffeeBeans() + " of coffee beans\n" +
-                getDisposableCups() + " of disposable cups\n" +
+                getDisposableCups() + " of disposable cups\n$" +
                 getMoney() + " of money");
     }
 }
-
-class Espresso extends Cups {
-
-    public Espresso(int water, int milk, int coffeeBeans) {
-        super(water, milk, coffeeBeans);
-        this.water = 250;
-        this.milk = 0;
-        this.coffeeBeans = 16;
-    }
-}
-
-class Latte extends Cups {
-
-    public Latte(int water, int milk, int coffeeBeans) {
-        super(water, milk, coffeeBeans);
-        this.water = 350;
-        this.milk = 75;
-        this.coffeeBeans = 20;
-    }
-}
-
-class Cappuccino extends Cups {
-
-    public Cappuccino(int water, int milk, int coffeeBeans) {
-        super(water, milk, coffeeBeans);
-        this.water = 200;
-        this.milk = 100;
-        this.coffeeBeans = 12;
-    }
-}
-
-//abstract class Cups {
-//    int waterOneCups;
-//    int milkOneCups;
-//    int coffeeBeansOneCups;
-//    int haveWater;
-//    int havemilk;
-//    int haveCoffeeBeans;
-//
-//    public Cups(int water, int milk, int coffeeBeans) {
-//        this.waterOneCups = 0;
-//        this.milkOneCups = 0;
-//        this.coffeeBeansOneCups = 0;
-//
-//        this.haveWater = water;
-//        this.havemilk = milk;
-//        this.haveCoffeeBeans = coffeeBeans;
-//    }
-//
-//    private boolean getOneCups() {
-//        boolean enough = true;
-//        if (haveWater - waterOneCups >= 0) {
-//            haveWater -= waterOneCups;
-//        } else {
-//            enough = false;
-//        }
-//        if (havemilk - milkOneCups >= 0) {
-//            havemilk -= milkOneCups;
-//        } else {
-//            enough = false;
-//        }
-//        if (haveCoffeeBeans - coffeeBeansOneCups >= 0) {
-//            haveCoffeeBeans -= coffeeBeansOneCups;
-//        } else {
-//            enough = false;
-//        }
-//        return enough;
-//    }
-//
-//    int getAllCups() {
-//        int cups = 0;
-//        while (getOneCups()) {
-//            cups++;
-//        }
-//        return cups;
-//    }
-//}
-//
-//class Espresso extends Cups {
-//
-//    public Espresso(int water, int milk, int coffeeBeans) {
-//        super(water, milk, coffeeBeans);
-//        this.waterOneCups = 250;
-//        this.milkOneCups = 0;
-//        this.coffeeBeansOneCups = 16;
-//    }
-//}
-//
-//class Latte extends Cups {
-//
-//    public Latte(int water, int milk, int coffeeBeans) {
-//        super(water, milk, coffeeBeans);
-//        this.waterOneCups = 350;
-//        this.milkOneCups = 75;
-//        this.coffeeBeansOneCups = 20;
-//    }
-//}
-//
-//class Cappuccino extends Cups {
-//
-//    public Cappuccino(int water, int milk, int coffeeBeans) {
-//        super(water, milk, coffeeBeans);
-//        this.waterOneCups = 200;
-//        this.milkOneCups = 100;
-//        this.coffeeBeansOneCups = 12;
-//    }
-//}
